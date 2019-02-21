af5fd20743d37999797fbc22a9072690f0c8e2e1
unknown <PC-REDES-DEV@DESKTOP-LS5N78K.carazo.unan.edu.ni>
2019-02-21 11:21:52 -0600

    Cuarto Commit

---
+++ src/components/piePagina.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class PieDePagina extends Component {
+  state = {};
+  render() {
+    return (
+      <section>
+        <article className="jumbotron">
+          <h1 />
+        </article>
+      </section>
+    );
+  }
+}
+
+export default PieDePagina;

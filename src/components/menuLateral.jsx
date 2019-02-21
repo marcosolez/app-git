29af0bc919611691aff856ba478e3fb392cfee9c
unknown <PC-REDES-DEV@DESKTOP-LS5N78K.carazo.unan.edu.ni>
2019-02-21 11:17:34 -0600

    Tercer Commit

---
+++ src/components/menuLateral.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class MenuLateral extends Component {
+  state = {};
+  render() {
+    return (
+      <ul>
+        <li>Hola</li>
+        <li>Que</li>
+        <li>Paso</li>
+      </ul>
+    );
+  }
+}
+
+export default MenuLateral;
